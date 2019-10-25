51c54380346203fda988c56d61b26305f1a14fa8
Arel-PC\Arel <arelguatno@yahoo.com>
2019-10-25 19:42:58 +0800

    asdasd

---
+++ public/index.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBuHMQ1zj6JvP_OpHibHdRcjr5vEQpOz2s",
+    authDomain: "sparktify-a6108.firebaseapp.com",
+    databaseURL: "https://sparktify-a6108.firebaseio.com",
+    projectId: "sparktify-a6108",
+    storageBucket: "sparktify-a6108.appspot.com",
+    messagingSenderId: "826003442822",
+    appId: "1:826003442822:web:d0754f767ef83ec98ad507",
+    measurementId: "G-M19LQM21DT"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
